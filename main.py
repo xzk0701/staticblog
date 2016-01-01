04cf47f669a711067e62be65acb18a01bfc10367
xzk0701 <xzk0701@qq.com>
2016-01-02 03:26:19 +0800

    firs day

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
 app=Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "hello,world!"
+def index(title):
+    return render_template('login.html')    
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=8080,debug=True)
 
